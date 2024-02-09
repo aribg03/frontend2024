fa87310732651c3a1f7f717dc527fc2f03da7c27
Arizbeth Becerra González <isc20350254@gmail.com>
2024-02-09 10:56:25 -0600

    Se agregó modulos

---
+++ modulos/src/App.jsx
@@ -0,0 +1,40 @@
+import ListItem from './Listitem.jsx'
+const links = [
+  {
+    name: 'Google',
+    url: 'https://www.google.com'
+  },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com'
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com'
+  }
+]
+
+function App() {
+
+  return (
+    <div>
+      <h1>Catálogo de Links</h1>
+      <hr />
+      <ul>
+        {
+          links.map((link) =>(
+            <ListItem 
+              key={link.name}
+              name={link.name}
+              url={link.url}/>
+            //<li key={link.name}>
+            //  <a href= {link.url}>{link.name}</a>
+            //</li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
